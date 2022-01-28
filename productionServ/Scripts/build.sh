dc3263a75c11ba6d978f6ad3e96295ff0de328e8
nikikone <nikitaz0614@gmail.com>
2022-01-29 04:04:44 +1000

    аддед Скриптс

---
+++ productionServ/Scripts/build.sh
@@ -0,0 +1,4 @@
+cd ..
+git pull origin master
+pip install -r requirements.txt
+cd Scripts
---
+++ productionServ/Scripts/deploy_dev.sh
@@ -0,0 +1,8 @@
+cd ..
+
+pip freeze > requirements.txt
+git add .
+git commit -m "Deployed development"
+git push origin master
+
+ssh -i C:/Users/Nikita/.ssh/id_rsa nikikone@159.89.232.171 'sh ~/server_production/SIT_laba6/Scripts/build.sh'
